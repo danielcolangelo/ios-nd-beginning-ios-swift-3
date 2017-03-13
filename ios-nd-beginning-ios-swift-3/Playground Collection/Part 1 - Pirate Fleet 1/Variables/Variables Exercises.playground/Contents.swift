bec21b5f2dada567f6cd79ae4be5e8f5991c18f1
Daniel Colangelo <Danalfons@yahoo.com>
2017-03-13 08:31:50 -0400

    problem set for swift 3 iOS nd

---
+++ ios-nd-beginning-ios-swift-3/Playground Collection/Part 1 - Pirate Fleet 1/Variables/Variables Exercises.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: ## Variables Exercises
+//: In these exercises, you will create variables and constants, and you will handle simple compiler areas that arise when working with them.
+/*:
+### Exercise 1
+
+For each of the following names and types, create a variables or constant that best represents it:
+
+- boilingPointOfWaterInCelsius (Double)
+- gallonsRemainingInTank (Double)
+- naturalLogarithmicBase (Double)
+- ageOfSpecimenInWeeks (Int)
+- studentNickname (String)
+*/
+let boilingPointOfWaterInCelsius = 12.5
+var gallonsRemainingInTank = 4.5
+let NaturalLogrithmicBase = 2.5
+var ageOfSpecimenInWeeks: Double = 4
+let studentNickname = "Dave"
+//: ### Exercise 2
+//: Fix the following compiler errors related to assignment, variables, and constants:
+var favoriteLanguage: String = "Swift"
+gallonsRemainingInTank = 2
+ageOfSpecimenInWeeks = 32.45
+let _numberOfGasIcons: Int = Int(gallonsRemainingInTank)
+var numberOfPolygonsInModel = 30
+numberOfPolygonsInModel = 32
+var polygonsLeftToDraw = 0
+polygonsLeftToDraw = numberOfPolygonsInModel
+
+//: ### Exercise 3
+//: Fix the following compiler errors related to invalid names:
+var nameOfSchool = "Udacity"
+var favoriteClass = "Math"
+let importCar = "Nissian"
+var 🐩myDog = "🐶"
+var 🐕yourDog = "🐶"
+
+
+
